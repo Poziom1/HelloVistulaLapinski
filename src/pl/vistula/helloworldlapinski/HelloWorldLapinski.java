87d00ba6eeb4b56b2de3799b01d687c39a92a867
MIKI <mlapins3@stu.vistula.edu.pl>
2022-10-07 19:03:39 +0200

    Mała zmiana

--- src/pl/vistula/helloworldlapinski/HelloWorldLapinski.java
+++ src/pl/vistula/helloworldlapinski/HelloWorldLapinski.java
@@ -3,6 +3,6 @@ package pl.vistula.helloworldlapinski;
 public class HelloWorldLapinski {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Łapiński");
-        System.out.println("Hello Vistula Łapiński 2");
+
     }
 }
