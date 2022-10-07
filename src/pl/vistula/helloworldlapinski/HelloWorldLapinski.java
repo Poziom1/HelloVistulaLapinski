bbedb9090ad09f60854bedcb36417546c1561526
MIKI <mlapins3@stu.vistula.edu.pl>
2022-10-07 18:55:20 +0200

    Moja praca

---
+++ src/pl/vistula/helloworldlapinski/HelloWorldLapinski.java
@@ -0,0 +1,8 @@
+package pl.vistula.helloworldlapinski;
+
+public class HelloWorldLapinski {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Łapiński");
+        System.out.println("Hello Vistula Łapiński 2");
+    }
+}
