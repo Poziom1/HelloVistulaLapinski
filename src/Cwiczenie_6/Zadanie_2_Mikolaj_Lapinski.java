6248aa31b6d0d3496b2a8bb2d4c6aad91570ddaa
MIKI <mlapins3@stu.vistula.edu.pl>
2022-11-23 16:56:54 +0100

    Mała zmiana

--- src/Cwiczenie_6/Zadanie_2_Mikolaj_Lapinski.java
+++ src/Cwiczenie_6/Zadanie_2_Mikolaj_Lapinski.java
@@ -1,16 +1,19 @@
 package Cwiczenie_6;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Zadanie_2_Mikolaj_Lapinski {
     public static void main(String[] args) {
 
+
     }
 }
 class Animal {
-    String []name = new String[100];
-    String [] age= new String[100];
-    String weight;
+    static Scanner scanner = new Scanner(System.in);
+    static String []name = new String[100];
+    static String [] age= new String[100];
+    static String [] weight = new String[100];
     static  void eat(){
         System.out.println("Hej");
 
@@ -21,25 +24,38 @@ class Animal {
 
 }
 class Mammal extends Animal{
-    String IloscNug;
-    static void pies(){
+    static String IloscNug;
 
+    static void pies(){
+        System.out.println("Wpisz ilośc nug: ");
+        IloscNug = scanner.next();
     }
 }
-class Dog extends Mammal{
-    String Breed;
+class Dog extends Mammal {
+    static String [] Breed = new String[100];
+    static int i = 1;
+    static void Breed(){
+
+        System.out.println("Wpisz rasę: ");
+        Breed [i] = scanner.next();
+        i++;
+    }
+    static void odczyt(){
+
+    }
 
 }
 class Bird extends Animal{
-    String featherColor;
+    static String featherColor;
+
 
 }
 class Pigeon extends Bird{
-    String species;
+    static String species;
 }
 class Fish extends Animal{
-    String species;
+    static String species;
 }
 class Blowfish extends Fish{
-    String nadmuchanie;
+    static String nadmuchanie;
 }
