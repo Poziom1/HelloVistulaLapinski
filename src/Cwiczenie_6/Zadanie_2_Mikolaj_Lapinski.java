c721c38893bc1b3763be7659d38f209b9b0acdce
MIKI <mlapins3@stu.vistula.edu.pl>
2022-11-25 00:23:26 +0100

    Mała zmiana

--- src/Cwiczenie_6/Zadanie_2_Mikolaj_Lapinski.java
+++ src/Cwiczenie_6/Zadanie_2_Mikolaj_Lapinski.java
@@ -4,8 +4,180 @@ import javax.swing.*;
 import java.util.Scanner;
 
 public class Zadanie_2_Mikolaj_Lapinski {
+    static Scanner scanner = new Scanner(System.in);
+    static int wybur;
+    static int p,g,r, j= 0;
+    static boolean wybór;
+    static String wybórS;
+    static boolean zakończ = false;
     public static void main(String[] args) {
+        System.out.println("Witam "+System.getProperty("user.name")+", co dzisiaj robimy?");
+        do{
+            System.out.println("0. Wyjście.");
+            System.out.println("1. Cała lista zwierząt.");
+            System.out.println("2. Lista psów.");
+            System.out.println("3. Zarejestruj psa.");
+            System.out.println("4. Lista gołębi.");
+            System.out.println("5. Zarejestruj gołębia.");
+            System.out.println("6. Lista rozdymek.");
+            System.out.println("7. Zarejestruj rozdymek.");
+            System.out.println("Resta w trakcie robienia.");
+            wybur = scanner.nextInt();
+            switch (wybur){
+                case 0:
+                    zakończ = true;
+                    break;
+                case 1:
+                    System.out.println("Psy:");
+                    if(p>=1){
 
+                        Dog.odczyt(p);
+                    }else{
+                        System.out.println("Nie ma listy.");
+                    }
+                    System.out.println("Gołębie:");
+                    if (g>=1){
+
+                        Pigeon.odczyt(g);
+                    }else{
+                        System.out.println("Nie ma listy.");
+                    }
+                    System.out.println("Rozdymki:");
+                    if (r>=1){
+
+                        Blowfish.odczyt(r);
+                    }else{
+                        System.out.println("Nie ma listy.");
+                    }
+                    break;
+                case 2:
+                    if(p>=1) {
+
+                        System.out.println("Czy chcesz wyświetlić całą listę?");
+                        wybórS = scanner.next();
+                        if(wybórS.contains("t")||wybórS.contains("y")){
+                            wybór = true;
+                        } else {
+                            wybór = false;
+                        }
+                        if (wybór) {
+                            System.out.println("Wybierz, wybierz z jakiego przedziału, jeżeli chcesz całą to "+p+" jest zarejestrowanych zwieźąt:");
+                            j = scanner.nextInt();
+                            Dog.odczyt(p,j);
+                        } else {
+                            System.out.println("Wybierz, numer zwieźęcia: ");
+                            j = scanner.nextInt();
+                            Dog.odczytPojedynczy(j);
+                        }
+                    }else {
+                        System.out.println("Nie ma listy.");
+                    }
+                    break;
+                case 3:
+                    do{
+                    System.out.println("Ile chcesz zarejestrować zwieżąt?");
+                    j=scanner.nextInt();
+                    for (int licz = 0;j>licz;licz++){
+                        Dog.Zapisz(p);
+                        Dog.odczytPojedynczy(p);
+                        p++;
+                    }
+                    System.out.println("Czy napewno wszystkie?");
+                    wybórS = scanner.next();
+                        if(wybórS.contains("t")||wybórS.contains("y")){
+                            wybór = true;
+                        } else {
+                            wybór = false;
+                        }
+                    } while (!wybór);
+
+                    break;
+                case 4:
+                    if(g>=1) {
+
+                        System.out.println("Czy chcesz wyświetlić całą listę?");
+                        wybórS = scanner.next();
+                        if(wybórS.contains("t")||wybórS.contains("y")){
+                            wybór = true;
+                        } else {
+                            wybór = false;
+                        }
+                        if (wybór) {
+                            System.out.println("Wybierz, wybierz z jakiego przedziału, jeżeli chcesz całą to "+g+" jest zarejestrowanych zwieźąt:");
+                            j = scanner.nextInt();
+                            Pigeon.odczyt(g,j);
+                        } else {
+                            System.out.println("Wybierz, numer zwieźęcia: ");
+                            j = scanner.nextInt();
+                            Pigeon.odczytPojedynczy(j);
+                        }
+                    }else {
+                        System.out.println("Nie ma listy.");
+                    }
+                    break;
+                case 5:
+
+                    do{
+                        System.out.println("Ile chcesz zarejestrować zwieżąt?");
+                        j=scanner.nextInt();
+                        for (int licz = 0;j>licz;licz++){
+                            Pigeon.Zapisz(g);
+                            Pigeon.odczytPojedynczy(g);
+                            g++;
+                        }
+                        System.out.println("Czy napewno wszystkie?");
+                        wybórS = scanner.next();
+                        if(wybórS.contains("t")||wybórS.contains("y")){
+                            wybór = true;
+                        } else {
+                            wybór = false;
+                        }
+                    } while (!wybór);
+                    break;
+                case 6:
+                    if(r>=1) {
+
+                        System.out.println("Czy chcesz wyświetlić całą listę?");
+                        wybórS = scanner.next();
+                        if(wybórS.contains("t")||wybórS.contains("y")){
+                            wybór = true;
+                        } else {
+                            wybór = false;
+                        }
+                        if (wybór) {
+                            System.out.println("Wybierz, wybierz z jakiego przedziału, jeżeli chcesz całą to "+r+" jest zarejestrowanych zwieźąt:");
+                            j = scanner.nextInt();
+                            Blowfish.odczyt(r,j);
+                        } else {
+                            System.out.println("Wybierz, numer zwieźęcia: ");
+                            j = scanner.nextInt();
+                            Blowfish.odczytPojedynczy(j);
+                        }
+                    }else {
+                        System.out.println("Nie ma listy.");
+                    }
+                    break;
+                case 7:
+
+                    do{
+                        System.out.println("Ile chcesz zarejestrować zwieżąt?");
+                        j=scanner.nextInt();
+                        for (int licz = 0;j>licz;licz++){
+                            Blowfish.Zapisz(r);
+                            Blowfish.odczytPojedynczy(r);
+                            r++;
+                        }
+                        System.out.println("Czy napewno wszystkie?");
+                        wybórS = scanner.next();
+                        if(wybórS.contains("t")||wybórS.contains("y")){
+                            wybór = true;
+                        } else {
+                            wybór = false;
+                        }
+                    } while (!wybór);
+                    break;
+            }
+        }while(!zakończ);
 
     }
 }
@@ -66,9 +238,12 @@ class Dog extends Mammal {
         eat(i);
         pies(i);
     }
+    static void odczytPojedynczy( int j){
+        System.out.println((j+1)+") "+name[j]+" "+age[j]+" "+Breed [j]+" "+weight[j]+" "+jedzenie [j]+" "+IloscNug[j]);
+    }
     static void odczyt(int i){
         for (int j = 0; i>j;j++){
-            System.out.println((j+1)+") "+name[j]+" "+age[j]+" "+Breed [j]+" "+weight[j]+" "+jedzenie [j]+" "+IloscNug[i]);
+            System.out.println((j+1)+") "+name[j]+" "+age[j]+" "+Breed [j]+" "+weight[j]+" "+jedzenie [j]+" "+IloscNug[j]);
         }
 
     }
@@ -88,7 +263,7 @@ class Dog extends Mammal {
 }
 class Bird extends Animal{
     static String jedzenie [] = new String[100];
-    static String [] featherColor;
+    static String [] featherColor = new String[100];
     static void kolorPiur( int i){
         System.out.println("Napisz kolor upierzenia zwierzaka: ");
         featherColor[i] = scanner.next();
@@ -96,7 +271,11 @@ class Bird extends Animal{
 
 }
 class Pigeon extends Bird{
-    static String [] species;
+    static String [] species = new String[100];
+    static void wysokosc( int i){
+        System.out.println("Napisz wysokość zwierzaka: ");
+        weight[i] = scanner.next();
+    }
     static void gatunek (int i){
         System.out.println("Napisz jaki to gatunek: ");
         species[i] = scanner.next();
@@ -114,12 +293,16 @@ class Pigeon extends Bird{
         jedzenie [i] = scanner.next();
     }
     static void Zapisz(int i){
-        System.out.println("Nowy pies");
+        System.out.println("Nowy gołąb");
         imie(i);
         wiek(i);
+        wysokosc(i);
         gatunek(i);
         kolorPiur(i);
         eat(i);
+    }
+    static void odczytPojedynczy(int j){
+        System.out.println((j+1)+") "+name[j]+" "+age[j]+" "+species [j]+" "+weight[j]+" "+featherColor[j]+" "+jedzenie [j]);
     }
     static void odczyt(int i){
         for (int j = 0; i>j;j++){
@@ -146,7 +329,10 @@ class Fish extends Animal{
 }
 class Blowfish extends Fish{
     static String[] nadmuchanie = new String[100];
-
+    static void wysokosc( int i){
+        System.out.println("Napisz wysokość zwierzaka: ");
+        weight[i] = scanner.next();
+    }
     static void nadmuchiwanie (int i){
         System.out.println("Napisz czy sie nadmuchuje: ");
         nadmuchanie[i] = scanner.next();
@@ -168,12 +354,16 @@ class Blowfish extends Fish{
         jedzenie [i] = scanner.next();
     }
     static void Zapisz(int i){
-        System.out.println("Nowy pies");
+        System.out.println("Nowy rozdymka");
         imie(i);
         wiek(i);
+        wysokosc(i);
         gatunek(i);
         eat(i);
         nadmuchiwanie(i);
+    }
+    static void odczytPojedynczy(int j){
+        System.out.println((j+1)+") "+name[j]+" "+age[j]+" "+species [j]+" "+weight[j]+" "+jedzenie [j]+" "+nadmuchanie[j]);
     }
     static void odczyt(int i){
         for (int j = 0; i>j;j++){
